05cc53d49e31d13914e9b43a0050dc4ad8dc3f8d
Frankblation <124920027+Frankblation@users.noreply.github.com>
2024-07-10 18:45:05 -0500

    blindd

---
+++ Javascript_advanced/13-blind_user.js
@@ -0,0 +1,21 @@
+var user = {
+    hobby: "Calligraphy",
+    favoriteSport: "Hockey",
+    astrologicalSign: "Aries",
+    firstName: "Buillaume",
+    lastName: "Johns",
+    location: "Netherlands",
+    occupation: "Engineer"
+};
+
+
+function logWelcomeUser(welcomeString) {
+    console.log(`${welcomeString}, ${this.firstName}. Your occupation is: ${this.occupation}`);
+}
+
+
+var bindLogWelcomeUser = logWelcomeUser.bind(user);
+
+// test me
+bindLogWelcomeUser('Hello');
+Task 14: Simple Callback
